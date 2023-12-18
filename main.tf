d0c64d37a2bb819d0dddbd89666d2d85398c977a
Anisha Gupta <anisha1999.gupta@gmail.com>
2023-12-18 01:03:09 -0500

    terraform changes without unnecessary files

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+# RSA key of size 4096 bits
+resource "tls_private_key" "terraform_rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.terraform_rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content = tls_private_key.terraform_rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+resource "aws_instance" "public_terraform_instance" {
+  ami           = "ami-01103fb68b3569475"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.key_pair.key_name
+
+  tags = {
+    Name = "public_terraform_instance"
+  }
+}
